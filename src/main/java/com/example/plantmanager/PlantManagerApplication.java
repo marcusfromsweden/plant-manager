d9a48151572931dddfecf3ad53ae8f782079e30d
marcusfromsweden <marcusfromsweden@gmail.com>
2025-01-20 00:19:01 +0100

    app starts up

--- src/main/java/com/example/plantmanager/PlantManagerApplication.java
+++ src/main/java/com/example/plantmanager/PlantManagerApplication.java
@@ -1,7 +1,9 @@
 package com.example.plantmanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class PlantManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlantManagerApplication.class, args);
--- src/main/java/com/example/plantmanager/entity/Plant.java
+++ src/main/java/com/example/plantmanager/entity/Plant.java
@@ -1,7 +1,16 @@
 package com.example.plantmanager.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
     private String type;
     private String description;
