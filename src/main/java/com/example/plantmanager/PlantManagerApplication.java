edc81a45a3e4b02b6a20e6761c23ea69058dd4f2
marcusfromsweden <marcusfromsweden@gmail.com>
2025-01-20 16:25:04 +0100

    formatting all

--- src/main/java/com/example/plantmanager/PlantManagerApplication.java
+++ src/main/java/com/example/plantmanager/PlantManagerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlantManagerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PlantManagerApplication.class, args);
     }
