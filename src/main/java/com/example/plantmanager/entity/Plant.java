9f235a4f6110225705c32743730b2ebdf80cd2f5
Marcus <marcusfromsweden@gmail.com>
2025-01-23 17:20:38 +0100

    uped spring, changed to jakarta.persistence & pom fixes

--- src/main/java/com/example/plantmanager/entity/Plant.java
+++ src/main/java/com/example/plantmanager/entity/Plant.java
@@ -1,12 +1,11 @@
 package com.example.plantmanager.entity;
 
 import java.time.LocalDate;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Plant {
